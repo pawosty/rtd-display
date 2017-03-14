e716983fb03434b7efef7698691579acce183182
Frederick (Paul) Wostenberg <frederick.wostenberg@readytalk.com>
2017-03-14 14:27:29 -0600

    hackday hack

--- index.js
+++ index.js
@@ -12,10 +12,26 @@ FeedData.loadUnionStationData(function(cb) {
 			data.forEach(function(aTrip) {
 				if(aTrip.trip_update.stop_time_update.departure && aTrip.trip_update.stop_time_update.departure.time.low) {
 					var displayDate = new Date(aTrip.trip_update.stop_time_update.departure.time.low * 1000);
-					console.log("Route " + aTrip.trip_update.trip.route_id + " with trip id " + aTrip.trip_update.trip.trip_id + 
-					" leaving from " + aTrip.trip_update.trip.stop_id_name + " toward " + aTrip.trip_update.trip.headsign + " direction " + aTrip.trip_update.trip.direction_id + " at " + displayDate.toTimeString());
+					console.log("Route " + aTrip.trip_update.trip.route_id + 
+					" leaving from " + aTrip.trip_update.trip.stop_id_name + " toward " + aTrip.trip_update.trip.headsign  + " at " + displayDate.toTimeString());
 				}
 			});
 		}
 	});
+	setInterval(function(){
+			FeedData.getUnionStationVehicles(function(err, data) {
+		if(err) {
+			console.log("Problem getting data: " + err);
+		} else {
+			console.log("~~~~~~~~~LEAVING UNION STATION~~~~~~~~~~~~~~")
+			data.forEach(function(aTrip) {
+				if(aTrip.trip_update.stop_time_update.departure && aTrip.trip_update.stop_time_update.departure.time.low) {
+					var displayDate = new Date(aTrip.trip_update.stop_time_update.departure.time.low * 1000);
+					console.log("Route " + aTrip.trip_update.trip.route_id + 
+					" leaving from " + aTrip.trip_update.trip.stop_id_name + " toward " + aTrip.trip_update.trip.headsign  + " at " + displayDate.toTimeString());
+				}
+			});
+		}
+	});
+	}, 30000);
 });
