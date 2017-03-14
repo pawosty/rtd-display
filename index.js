2a53e038dab8e972f4611db3a0323ce1dfe6381a
Paul Wostenberg <pawosty@gmail.com>
2017-03-14 01:57:09 -0600

    cleanup

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ FeedData.loadUnionStationData(function(cb) {
 			console.log("~~~~~~~~~LEAVING UNION STATION~~~~~~~~~~~~~~")
 			data.forEach(function(aTrip) {
 				if(aTrip.trip_update.stop_time_update.departure && aTrip.trip_update.stop_time_update.departure.time.low) {
-					//console.log(" A TRIP: ");
-					//console.log(JSON.stringify(aTrip));
 					var displayDate = new Date(aTrip.trip_update.stop_time_update.departure.time.low * 1000);
 					console.log("Route " + aTrip.trip_update.trip.route_id + 
 					" leaving from " + aTrip.trip_update.trip.stop_id_name + " toward " + aTrip.trip_update.trip.headsign + " at " + displayDate.toTimeString());
