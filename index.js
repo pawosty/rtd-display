d3053a2c63cc12b2afeb5e8d36b6837fbc79aecd
Paul Wostenberg <pawosty@gmail.com>
2017-03-14 01:55:33 -0600

    Now cross-referencing live feed with stop/trip data

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ var FeedData = require('./lib/feedUtils.js')
 
 var getUnionStationVehicles = [];
 
-FeedData.getUnionStationVehicles(function(err, data) {
-
-	if(err) {
-		console.log("Problem getting data: " + err);
-	} else {
-		//console.log("trip data length: " + data.length)
-		console.log("Here is some data for a station:" + JSON.stringify(data));
-		console.log("~~~~~~~~~LEAVING UNION STATION~~~~~~~~~~~~~~")
-		data.forEach(function(aTrip) {
-			console.log("Route " + aTrip.trip_update.trip.route_id + " to " + aTrip. + " at " + );
-		});
-	}
+FeedData.loadUnionStationData(function(cb) {
+	console.log("Stop/trip data loaded");
+	FeedData.getUnionStationVehicles(function(err, data) {
+		if(err) {
+			console.log("Problem getting data: " + err);
+		} else {
+			console.log("~~~~~~~~~LEAVING UNION STATION~~~~~~~~~~~~~~")
+			data.forEach(function(aTrip) {
+				if(aTrip.trip_update.stop_time_update.departure && aTrip.trip_update.stop_time_update.departure.time.low) {
+					//console.log(" A TRIP: ");
+					//console.log(JSON.stringify(aTrip));
+					var displayDate = new Date(aTrip.trip_update.stop_time_update.departure.time.low * 1000);
+					console.log("Route " + aTrip.trip_update.trip.route_id + 
+					" leaving from " + aTrip.trip_update.trip.stop_id_name + " toward " + aTrip.trip_update.trip.headsign + " at " + displayDate.toTimeString());
+				}
+			});
+		}
+	});
 });
